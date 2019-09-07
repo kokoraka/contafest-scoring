d121e183d69670fbac08a62868771757f8809502
Raka Suryaardi Widjaja <raka.suryadi@gmail.com>
2019-09-07 17:01:16 +0700

    [ADD] offline ability

--- worker.js
+++ worker.js
@@ -1,7 +1,34 @@
-importScripts('/polyfill.js');
+var cache_version = 'v1';
+var cache_static = 's-confucius.id-' + cache_version;
+var cache_request = [
+  'index.html',
+  'index.js',
+  'index.css',  
+  'polyfill.js',
+  '/assets/jquery/jquery.min.js',
+  '/assets/vuejs/vue.js',
+  '/assets/bootstrap/js/bootstrap.min.js',
+  '/assets/babel/babel.min.js',
+  '/assets/jsstore/dist/jsstore.min.js',
+  '/assets/jsstore/dist/jsstore.worker.min.js',
+  '/services/connection.js',
+  '/services/team/Team.js',
+  '/services/battle/Battle.js',
+  '/services/battle/BattleTeam.js',
+  '/services/battle/BattleHistory.js',
+  '/assets/image/favicon.png',
+  '/assets/image/logo.png',
+];
 
 self.addEventListener('install', event => {
   console.log('Service worker installing...');
+  event.waitUntil(
+    caches.open(cache_static).then(function(cache) {
+      return cache.addAll(cache_request);
+    }).catch(function(error) {
+      console.log(error);
+    })
+   );
 });
 
 self.addEventListener('activate', event => {
